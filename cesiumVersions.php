9cb3f698d815783265c126082437653609870da1
vincentux <vincentux@vincentux.fr>
2020-04-23 08:05:27 +0000

    Update cesiumVersions.php

--- cesiumVersions.php
+++ cesiumVersions.php
@@ -1,13 +1,13 @@
 <?php
 
 $cesiumVersions = [
-	   'home' => '1.6.1', 
-	'android' => '1.6.1', 
-	  'linux' => '1.6.1', 
+	   'home' => '1.6.5', 
+	'android' => '1.6.5', 
+	  'linux' => '1.6.5', 
 	'windows' => '1.3.11', 
-	'firefox' => '1.6.1', 
+	'firefox' => '1.6.5', 
 	    'mac' => '1.3.11', 
-	    'web' => '1.6.1', 
+	    'web' => '1.6.5', 
 	
 ];
 	   
