f176838527900d4a6e673c8def54daca9102235e
Boris Paing <boris.paing@laposte.net>
2020-05-04 16:11:25 +0200

    ENH: changement d'ordre et remplacement logo Debian par logo Ubuntu

--- cesiumDownloads.php
+++ cesiumDownloads.php
@@ -17,34 +17,6 @@ $cesiumDownloads =
 		'desc' => _('Disponible sur l\'App Store'),
 		'extra' => ''
 	],
-	'debian' => [
-		'title' => 'Debian (et dérivés)',
-		'url' => sprintf('https://github.com/duniter/cesium/releases/download/v%s/cesium-desktop-v%s-linux-x64.deb', $cesiumVersions['debian'], $cesiumVersions['debian']),
-		'img' => 'debian.svg',
-		'desc' => _('Paquet .deb pour Debian ou Ubuntu'),
-		'extra' => _('En cas de problème&nbsp;:') . '<br /><code>sudo apt-get install -y libgconf-2-4</code>'
-	],
-		'arch' => [
-		'title' => 'Arch Linux (et dérivés)',
-		'url' => sprintf('https://aur.archlinux.org/packages/cesium-desktop-deb'),
-		'img' => 'arch.svg',
-		'desc' => _('A installer via AUR'),
-		'extra' => _('<code>yaourt -S cesium-desktop-deb</code>')
-	],
-	'windows' => [
-		'title' => 'Windows',
-		'url' => sprintf('https://github.com/duniter/cesium/releases/download/v%s/cesium-desktop-v%s-windows-x64.exe', $cesiumVersions['windows'], $cesiumVersions['windows']),
-		'img' => 'windows.svg',
-		'desc' => _('Installateur .exe pour Windows'),
-		'extra' => _('Lancez le .exe et suivez les instructions à l\'écran')
-	],
-	'mac' => [
-		'title' => 'Mac',
-		'url' => sprintf('https://github.com/duniter/cesium/releases/download/v%s/cesium-v%s-web.zip', $cesiumVersions['mac'], $cesiumVersions['mac']),
-		'img' => 'apple.svg',
-		'desc' => _('Version web fonctionnant sous Mac'),
-		'extra' => _('Décompressez le fichier .zip et ouvrez le fichier index.html')
-	],
 	'firefox' => [
 		'title' => _('Firefox'),
 		'url' => sprintf('https://github.com/duniter/cesium/releases/download/v%s/cesium-v%s-extension-firefox.xpi', $cesiumVersions['firefox'], $cesiumVersions['firefox']),
@@ -65,6 +37,34 @@ $cesiumDownloads =
 		'img' => 'brave_lion.svg',
 		'desc' => _('Extension pour Brave'),
 		'extra' => ''
+	],
+	'debian' => [
+		'title' => 'Ubuntu&nbsp;/ Debian',
+		'url' => sprintf('https://github.com/duniter/cesium/releases/download/v%s/cesium-desktop-v%s-linux-x64.deb', $cesiumVersions['debian'], $cesiumVersions['debian']),
+		'img' => 'cof_orange_hex.svg',
+		'desc' => _('Paquet .deb pour Debian et ses dérivés (Ubuntu, Linux Mint, etc.)'),
+		'extra' => _('En cas de problème&nbsp;:') . '<br /><code>sudo apt-get install -y libgconf-2-4</code>'
+	],
+	'arch' => [
+		'title' => 'Arch Linux (et dérivés)',
+		'url' => sprintf('https://aur.archlinux.org/packages/cesium-desktop-deb'),
+		'img' => 'arch.svg',
+		'desc' => _('A installer via AUR'),
+		'extra' => _('<code>yaourt -S cesium-desktop-deb</code>')
+	],
+	'windows' => [
+		'title' => 'Windows',
+		'url' => sprintf('https://github.com/duniter/cesium/releases/download/v%s/cesium-desktop-v%s-windows-x64.exe', $cesiumVersions['windows'], $cesiumVersions['windows']),
+		'img' => 'windows.svg',
+		'desc' => _('Installateur .exe pour Windows'),
+		'extra' => _('Lancez le .exe et suivez les instructions à l\'écran')
+	],
+	'mac' => [
+		'title' => 'Mac',
+		'url' => sprintf('https://github.com/duniter/cesium/releases/download/v%s/cesium-v%s-web.zip', $cesiumVersions['mac'], $cesiumVersions['mac']),
+		'img' => 'apple.svg',
+		'desc' => _('Version web fonctionnant sous Mac'),
+		'extra' => _('Décompressez le fichier .zip et ouvrez le fichier index.html')
 	],
 	'web' => [
 		'title' => 'Web',
