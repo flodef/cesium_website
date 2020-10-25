1f77e1ae6dbaba0385facfae98c31ad12c51bb63
BorisPAING <boris.paing@laposte.net>
2020-10-25 16:54:39 +0000

    3ème MàJ copywriting CTA don

--- tpl/funding-orig.php
+++ tpl/funding-orig.php
@@ -28,12 +28,14 @@ include('head.php');
 		
 			<p>
 				'. _('On profite de ce moment d\'attente pour vous adresser ce petit appel à l\'action&nbsp;:') .'
-			</p>';
+			</p>
 			
+			<h2>
+				'. _('On a besoin de vous&nbsp;!') . '
+			</h2>
+			';
 		}
 		?>
-		
-		<h2>On a besoin de vous !</h2>
 		
 		<p>
 			L'adoption de la Ğ1 est lente.
@@ -44,57 +46,101 @@ include('head.php');
 		</p>
 
 		<ol>
-			<li>il est difficile de convertir des gens à la monnaie libre, pour diverses raisons (la monnaie, c'est compliqué, la confiance n'est pas forcément là a prior, etc.)</li>
-			<li>les logiciels de l'écosystème (Cesium, Duniter, etc.) doivent encore être améliorés pour garantir l'expansion de la Ğ1 (par exemple en permettant de valider les transactions plus sûrement et rapidement)</li>
+			<li>il semble difficile de convertir des gens à la monnaie libre, pour diverses raisons (la monnaie, c'est compliqué, la confiance n'est pas forcément là a prior, etc.)</li>
+			<li>les logiciels de l'écosystème (Cesium, Duniter, etc.) doivent encore être améliorés pour garantir l'expansion de la Ğ1 (par exemple en permettant de valider les transactions plus sûrement et plus rapidement)</li>
 		</ol>
 
 		<h2>
 			Comment vous pouvez contribuer au développement de la Ğ1
 		</h2>
 		
-		<h3>
-			1. En finançant les développeurs en Ğ1
-		</h3>
-		
 		<p>
 			La situation actuelle est la suivante&nbsp;:
 		</p>
 
 		<p>
-			Certains développeurs de la Ğ1 (Eloïs par exemple) ont fait le choix de démissionner 
-			de leur emploi pour se concentrer sur le développement de l'écosystème logiciel.  
-			D'autres développeurs contribuent sur leur temps libre, les soirs et week-ends.
+			Certains développeurs de la Ğ1 (par exemple Eloïs, qui développe Duniter, le moteur 
+			de la blockchain) ont fait le choix de démissionner de leur emploi pour se concentrer 
+			sur le développement de l'écosystème logiciel de la Ğ1. Les autres développeurs, eux, 
+			ont choisi de sacrifier leur temps libre, souvent leurs soirs, voire leurs week-ends.
 		</p>
 
 		<p>
-			Cette situation ne permet pas un développement très rapide des logiciels 
+			Mais cette situation ne permet pas un développement très rapide des logiciels 
 			car, comme tout un chacun, un développeur n'a que 24&nbsp;h dans une journée.
 		</p>
 
 		<p>
-			Cependant, il existe des caisses de côtisations qui permettent à la communauté Ğ1 de valoriser, en Ğ1, 
+			Cependant, il y a deux moyens par lesquels vous pouvez contribuer à 
+			accélérer le processus&nbsp;:
+		</p>
+		
+		<h3>
+			Moyen n°1&nbsp;: Vendre, en Ğ1, des biens et services
+		</h3>
+		
+		<p>
+			La valeur d'une monnaie tient aux biens et services qui s'échangent dans cette monnaie.
+		</p>
+
+		<p>
+			Plus il y aura de biens et services en vente en Ğ1, plus la monnaie libre sera intéressante.
+		</p>
+		
+		<p>
+			En vendant un bien ou un service, vous permettez aux développeurs de continuer à travailler
+			à l'amélioration de la Ğ1, de deux manières&nbsp;:
+		</p>
+		
+		<ol>
+			<li>
+				<strong>directement</strong> si vous vendez un bien ou service à un développeur. 
+				À titre d'exemple, Éloïs, qui en 2020 est le développeur principal de Duniter 
+				(logiciel crucial car c'est le moteur de la blockchain)
+				<a href="https://www.gchange.fr/#/app/market/view/AXIVawVxUm73BnXnuMPx/cherche-logement-a-louer-toute-lanne-en-1-en-occitanie">cherche un logement à louer en 
+				Occitanie</a>.
+			</li>
+			<li>
+				<strong>indirectement</strong> (mais c'est tout aussi utile&nbsp;!) si vous vendez 
+				à quelqu'un qui ne développe pas la Ğ1, car tout utilisateur de la Ğ1 qui voit qu'il 
+				peut acheter des biens et services en Ğ1 sera à son tour motivé à mettre des choses 
+				en vente, et cela peut conduire à la mise en vente par certains junistes de biens ou 
+				services que les développeurs recherchent
+			</li>
+		</ol>
+
+		<h3>
+			Moyen n°2&nbsp;: En finançant, en Ğ1, les développeurs
+		</h3>
+
+		<p>
+			Il existe des caisses de côtisations qui permettent à la communauté Ğ1 de valoriser, en Ğ1, 
 			les contributions que les développeurs apportent à l'écosystème logiciel de la Ğ1.
 		</p>
 		
 		<p>
-			Chaque mois, une vingtaine de contributeurs se voient gratifiés d'au moins 15 DU<sub>Ğ1</sub> pour leurs travaux sur Cesium, Silkaj, Sakia, Duniter, etc... Il est donné davantages à certains contributeurs, sur divers critères comme l'impact espéré de leur contribution, ou encore la quantité de travail nécessaire à la production du code.
+			Chaque mois, une vingtaine de contributeurs se voient gratifiés d'au moins 15 DU<sub>Ğ1</sub> 
+			pour leurs travaux sur Cesium, Silkaj, Sakia, Duniter, etc... Il est donné davantage à certains 
+			contributeurs, sur divers critères, comme l'impact espéré de leur contribution, ou encore la quantité 
+			de travail nécessaire à la production du code.
 		</p>
 		
 		<p>
-			Ces rémunérations sont faites en toute transparence ; et vous pouvez les retrouver sur le site et le forum de Duniter, ainsi que dans la blockchain.
+			Ces rémunérations sont faites en toute transparence ; et vous pouvez les retrouver 
+			sur le site et le forum de Duniter, ainsi que dans la blockchain.
 		</p>
 		
 		<p>
 			Nous aimerions augmenter progressivement la rémunération des développeurs 
-			jusqu'à atteindre des montants qui permettent à quelques développeurs 
-			d'allouer à la Ğ1 davantage de leur temps.
+			jusqu'à atteindre des montants qui permettent à ceux qui ont quitté leur 
+			ancien travail (celui qui était rémunéré en monnaie non-libre) de continuer 
+			à travailler à l'amélioration de la Ğ1 sur le long terme.
 		</p>
 		
 		<p>
 			Ce mois-ci, nous aimerions donc atteindre la somme de <?php echo FUNDING_TARGET; ?> DU<sub>Ğ1</sub>. 
 			Voilà où nous en sommes par rapport à cet objectif&nbsp;:
 		</p>
-
 		
 		<?php
 		include('inc/Crowdfunding.class.php');
@@ -156,9 +202,8 @@ include('head.php');
 		
 		?>
 		
-		
 		<p>
-			Si vous souhaitez soutenir la Ğ1, c'est simple : 
+			Si vous souhaitez soutenir la Ğ1, c'est simple&nbsp;: 
 		</p>
 
 		<div id="pubkey-and-copy-button">
@@ -182,35 +227,6 @@ include('head.php');
 			</div>
 		</div>
 		
-		<h3>
-			2. En vendant en Ğ1 des biens et services
-		</h3>
-		
-		<p>
-			La valeur d'une monnaie tient aux biens et services qui s'échange dans ladite monnaie.
-		</p>
-
-		<p>
-			Pour valoriser la Ğ1, il faut mettre des biens et services en vente, par exemple sur 
-			<a href="https://www.gchange.fr/">ğchange</a>.
-		</p>
-		
-		<p>
-			En faisant ça, vous permettez aux développeurs de continuer à développer la Ğ1 de deux façons&nbsp;:
-		</p>
-		
-		<ol>
-			<li>
-				<strong>directement</strong> si vous vendez un bien ou service à un développeur. À titre d'exemple, Éloïs, 
-				qui en 2020 est le développeur principal de Duniter (logiciel crucial car c'est le moteur de la blockchain)
-				<a href="https://www.gchange.fr/#/app/market/view/AXIVawVxUm73BnXnuMPx/cherche-logement-a-louer-toute-lanne-en-1-en-occitanie">cherche un logement à louer en Occitanie</a>.
-			</li>
-			<li>
-				<strong>indirectement</strong> (mais c'est tout aussi utile&nbsp;!) si vous vendez à quelqu'un qui ne développe pas la Ğ1, 
-				car tout utilisateur de la Ğ1 qui voit qu'il peut acheter des biens et services en Ğ1 sera à son tour motivé à mettre des choses 
-				en vente, et cela peut conduire à la mise en vente par certains junistes de biens ou services que les développeurs recherchent
-			</li>
-		</ol>
 
 		<?php
 		/*
