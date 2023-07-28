ed29b6a6a4ea0b758bf1ad5627bac00ff418fcc0
Flojito Stillnet <flo@fims.fi>
2023-07-28 18:23:49 +0200

    Update cesiumVersions.php
    Update to 1.7.3 for android and web versions

--- cesiumVersions.php
+++ cesiumVersions.php
@@ -1,10 +1,10 @@
 <?php
 
 $cesiumVersions = [
-	   'home' => '1.6.12',
-	'android' => '1.6.12',
+	   'home' => '1.7.3',
+	'android' => '1.7.3',
 	 'debian' => '1.6.12',
  	   'arch' => '1.6.1', 
 	'windows' => '1.6.12',
-	    'web' => '1.6.12'
+	    'web' => '1.7.3'
 ];
